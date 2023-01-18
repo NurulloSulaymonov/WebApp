99b6f9e9ded876bb40fb53cadc3166619ba7c25b
Nurullo Sulaymonov <48081398+NurulloSulaymonov@users.noreply.github.com>
2023-01-18 16:59:10 +0500

    test

---
+++ Domain/Entities/Customer.cs
@@ -0,0 +1,13 @@
+namespace Domain.Entities;
+
+public class Customer
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+}
+
+
---
+++ WebApi/Program.cs
@@ -0,0 +1,38 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<DataContext>(opt => opt.UseNpgsql(connection));
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
